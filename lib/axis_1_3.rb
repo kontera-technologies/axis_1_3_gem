61db19de104b5b6b851757e1f26c505c7746a0b0
Roee Sarusi <roee@kontera.com>
2013-09-08 17:05:27 +0200

    Wrapped the WSDL2Java script as a module method: wsdl_to_java.

--- lib/axis_1_3.rb
+++ lib/axis_1_3.rb
@@ -5,6 +5,11 @@ module Axis_1_3
 		path = File.expand_path("../../", __FILE__)
 		(Dir["#{path}/java_lib/\*.jar"] + Dir["#{path}/java_lib/axis-1_3/*.jar"])
 	end
+
+	def Axis_1_3.wsdl_to_java(*args)
+		puts `java -cp #{Axis_1_3::JAR_LIST.join(':')} org.apache.axis.wsdl.WSDL2Java #{args.join(' ')}`
+		$?
+	end
 end
 
 # load jars for lib folders
